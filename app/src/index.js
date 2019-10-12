da80cd54915cd2e66d704dc9168195ef64638f50
Andrew Stanger <andrew@stanger.co.uk>
2019-10-12 11:29:12 +0900

    Now displays on non metamask browser

--- app/src/index.js
+++ app/src/index.js
@@ -37,16 +37,21 @@ const App = {
 
         try {
             // get contract instance
-            const networkId = await web3.eth.net.getId();
-            const deployedNetwork = DrewsReviewsArtifact.networks[networkId];
-            this.instance = new web3.eth.Contract(
-                DrewsReviewsArtifact.abi,
-                deployedNetwork.address,
-            );
 
-            // get accounts
-            const accounts = await web3.eth.getAccounts();
-            this.account = accounts[0];
+            try {
+                const networkId = await web3.eth.net.getId();
+                const deployedNetwork = DrewsReviewsArtifact.networks[networkId];
+                this.instance = new web3.eth.Contract(
+                    DrewsReviewsArtifact.abi,
+                    deployedNetwork.address,
+                );
+
+                // get accounts
+                const accounts = await web3.eth.getAccounts();
+                this.account = accounts[0];
+                } catch (error) {
+                    console.error("Could not connect to contract or chain1.");
+                }
 
             this.renderHeader();
 
@@ -111,7 +116,7 @@ const App = {
             });
 
         } catch (error) {
-            console.error("Could not connect to contract or chain.");
+            console.error("Could not connect to contract or chain2.");
         }
     },
 
@@ -340,12 +345,12 @@ window.addEventListener("load", function() {
         window.ethereum.enable(); // get permission to access accounts
     } else {
         console.warn(
-            "No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",
+            "No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live or there will be trouble",
         );
         // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-        App.web3 = new Web3(
-            new Web3.providers.HttpProvider("http://127.0.0.1:9545"),
-        );
+        // App.web3 = new Web3(
+        //     new Web3.providers.HttpProvider("http://127.0.0.1:9545"),
+        // );
     }
 
     App.start();
